ea434523f12135152b553fb64060ae34977a53f6
Filip Ferm <ferm.filip@gmail.com>
2015-04-28 14:54:31 +0200

    Basic setup for prototype code
    indicator or mouse press, tells x and y coordinates on mouse when
    pressed.

--- src/main/java/com/khamekaze/fejkathegame/game/Game.java
+++ src/main/java/com/khamekaze/fejkathegame/game/Game.java
@@ -3,28 +3,61 @@ package com.khamekaze.fejkathegame.game;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.MouseListener;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Circle;
 
 public class Game extends BasicGame implements MouseListener {
-    
+
+    private Circle indicator;
+    private float mouseX, mouseXBefore, y, mouseY, mouseYBefore, mouseSpeed;
+    private boolean attack;
+
     public Game(String gameName) {
         super(gameName);
     }
 
     @Override
     public void init(GameContainer gc) throws SlickException {
-        
+
+        mouseY = gc.getHeight() / 2;
+        mouseX = gc.getWidth() / 2;
+
+        attack = false;
+        mouseSpeed = 0;
+
+        mouseXBefore = mouseX;
+        mouseYBefore = mouseY;
+
+        indicator = new Circle(mouseX, gc.getWidth() / 2, 30);
     }
 
     @Override
     public void update(GameContainer gc, int i) throws SlickException {
-        
+        Input input = gc.getInput();
+
+        mouseX = input.getMouseX();
+        mouseY = input.getMouseY();
+
+        if (input.isMousePressed(Input.MOUSE_RIGHT_BUTTON)) {
+            mouseXBefore = input.getMouseX();
+            mouseYBefore = input.getMouseY();
+
+            indicator.setCenterX(mouseXBefore);
+            indicator.setCenterY(mouseYBefore);
+            System.out.println("MouseX: " + mouseX + " MouseY: " + mouseY);
+        }
     }
 
     @Override
-    public void render(GameContainer gc, Graphics grphcs) throws SlickException {
-        
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+        drawOriginIndicator(g, gc);
     }
 
+    public void drawOriginIndicator(Graphics g, GameContainer gc) {
+        if (gc.getInput().isMouseButtonDown(Input.MOUSE_RIGHT_BUTTON)) {
+            g.draw(indicator);
+        }
+    }
 }
