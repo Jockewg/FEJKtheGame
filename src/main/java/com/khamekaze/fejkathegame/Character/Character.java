e01f4a91bb924bff6faa349eda13740346abc6d7
swartt <bobbyfeta@hotmail.com>
2015-04-28 15:59:04 +0200

    Updated Character class

--- src/main/java/com/khamekaze/fejkathegame/Character/Character.java
+++ src/main/java/com/khamekaze/fejkathegame/Character/Character.java
@@ -1,5 +1,10 @@
 package com.khamekaze.fejkathegame.Character;
 
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Shape;
+
 /**
  * Created by Swartt on 2015-04-28.
  */
@@ -9,11 +14,18 @@ public class Character {
     private float attackCoolDown;
     private int storedAttacks;
     private int storedJumps;
-    private int jumpCoolDown;
+    private float jumpStrength;
     private float size;
-    private float velocityY, velocityX, currentPositionX, currentPositionY;
+    private float velocityY;
+    private float velocityX;
+    private float currentPositionX;
+    private float currentPositionY;
     private boolean isAlive;
     private HealthSystem healthSystem;
+    private MovementSystem movementSystem;
+    private Shape player;
+    private float gravity;
+    private Color color;
 
     /**
      * Constructor for creating a character, gives it the default values for a character
@@ -24,10 +36,14 @@ public class Character {
         attackCoolDown = 1;
         storedAttacks = 2;
         storedJumps = 2;
-        jumpCoolDown = 1;
         size = 40;
+        velocityY = 0;
+        velocityX = 0;
         isAlive = true;
         this.healthSystem = new HealthSystem(this);
+        this.movementSystem = new MovementSystem(this);
+        gravity = 0.5f;
+        jumpStrength = -15;
     }
 
     public boolean isGrounded() {
@@ -68,14 +84,6 @@ public class Character {
 
     public void setStoredJumps(int storedJumps) {
         this.storedJumps = storedJumps;
-    }
-
-    public int getJumpCoolDown() {
-        return jumpCoolDown;
-    }
-
-    public void setJumpCoolDown(int jumpCoolDown) {
-        this.jumpCoolDown = jumpCoolDown;
     }
 
     public float getSize() {
@@ -128,5 +136,61 @@ public class Character {
 
     public HealthSystem getHealthSystem() {
         return healthSystem;
+    }
+
+    public MovementSystem getMovementSystem() {
+        return movementSystem;
+    }
+
+    public Shape getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Shape player) {
+        this.player = player;
+    }
+
+    public float getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(float gravity) {
+        this.gravity = gravity;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public float getJumpStrength() {
+        return jumpStrength;
+    }
+
+    public void setJumpStrength(float jumpStrength) {
+        this.jumpStrength = jumpStrength;
+    }
+
+    public void init(GameContainer gc) throws SlickException {
+        player = new Circle(200, 200, size);
+    }
+
+
+    public void update(GameContainer gc, int i) throws SlickException {
+        movementSystem.gravity();
+
+        if (gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+            movementSystem.jump();
+        }
+
+    }
+
+
+    public void render(GameContainer gc, Graphics grphcs) throws SlickException {
+        grphcs.setColor(color);
+        grphcs.draw(player);
     }
 }
