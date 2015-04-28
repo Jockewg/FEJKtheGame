5a4f72fd87ef3cfa178341ff5e1ac174c9584593
swartt <bobbyfeta@hotmail.com>
2015-04-28 12:14:09 +0200

    Created A character template

---
+++ src/main/java/com/khamekaze/fejkathegame/Character/Model.java
@@ -0,0 +1,26 @@
+package com.khamekaze.fejkathegame.Character;
+
+import org.newdawn.slick.geom.Rectangle;
+
+/**
+ * Created by Swartt on 2015-04-28.
+ */
+public class Model {
+    private boolean gournded;
+    private int health;
+    private float attackCoolDown;
+    private int storedAttacks;
+    private int storedJumps;
+    private int jumpCoolDown;
+    private float size;
+
+    /**
+     * Constructor for
+     */
+    public Model ()  {
+
+
+
+    }
+
+}
