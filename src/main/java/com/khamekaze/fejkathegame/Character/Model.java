1e84ac2eef5f152ecf1a7851af74cec2f2ae284a
swartt <bobbyfeta@hotmail.com>
2015-04-28 12:17:07 +0200

    Added A constructor and Getter and Setter methods

--- src/main/java/com/khamekaze/fejkathegame/Character/Model.java
+++ src/main/java/com/khamekaze/fejkathegame/Character/Model.java
@@ -6,7 +6,7 @@ import org.newdawn.slick.geom.Rectangle;
  * Created by Swartt on 2015-04-28.
  */
 public class Model {
-    private boolean gournded;
+    private boolean grounded;
     private int health;
     private float attackCoolDown;
     private int storedAttacks;
@@ -15,12 +15,71 @@ public class Model {
     private float size;
 
     /**
-     * Constructor for
+     * Constructor for creating a character, gives it the default values for a character
      */
     public Model ()  {
-
-
-
+    grounded = false;
+        health = 5;
+        attackCoolDown = 1;
+        storedAttacks = 2;
+        storedJumps = 2;
+        jumpCoolDown = 1;
+        size = 40;
     }
 
+    public boolean isGrounded() {
+        return grounded;
+    }
+
+    public void setGrounded(boolean grounded) {
+        this.grounded = grounded;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public float getAttackCoolDown() {
+        return attackCoolDown;
+    }
+
+    public void setAttackCoolDown(float attackCoolDown) {
+        this.attackCoolDown = attackCoolDown;
+    }
+
+    public int getStoredAttacks() {
+        return storedAttacks;
+    }
+
+    public void setStoredAttacks(int storedAttacks) {
+        this.storedAttacks = storedAttacks;
+    }
+
+    public int getStoredJumps() {
+        return storedJumps;
+    }
+
+    public void setStoredJumps(int storedJumps) {
+        this.storedJumps = storedJumps;
+    }
+
+    public int getJumpCoolDown() {
+        return jumpCoolDown;
+    }
+
+    public void setJumpCoolDown(int jumpCoolDown) {
+        this.jumpCoolDown = jumpCoolDown;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
 }
