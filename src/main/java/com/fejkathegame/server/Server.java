fee261f1b7467ffa421c9776c0573145585148fb
swartt <bobbyfeta@hotmail.com>
2015-05-01 10:23:07 +0200

    Updated server

--- src/main/java/com/fejkathegame/server/Server.java
+++ src/main/java/com/fejkathegame/server/Server.java
@@ -1,46 +1,82 @@
 package com.fejkathegame.server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.fejkathegame.game.Character.Character;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Created by Swartt on 2015-04-30.
  */
 public class Server {
-    protected static ServerSocket serverSocket;
-    protected static Socket clientSocket;
-    protected static BufferedReader bufferedReader;
-    protected static String inputLine;
-    protected static int port = 6112;
+    protected ServerSocket serverSocket;
+    protected Socket clientSocket;
+    protected BufferedReader bufferedReader;
+    protected String inputLine;
+    protected int port = 6112;
+    protected ArrayList<Character> players = new ArrayList<>();
 
 
-    public static void main(String[] args) {
-        connect();
-    }
+    public void connect() {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
 
-
-    public static void connect() {
+          /*  bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));*/
         try {
             serverSocket = new ServerSocket(port);
-
-            clientSocket = serverSocket.accept();
-
-            // Create a reader
-
-            bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            // Get the client message
-
-            while ((inputLine = bufferedReader.readLine()) != null)
-
-                System.out.println(inputLine);
         } catch (IOException e) {
             System.out.println(e);
         }
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            new ServerThread(clientSocket).start();
+        }
+
+
     }
 
-
 }
+
+class ServerThread extends Thread {
+    protected Socket clientSocket;
+
+    public ServerThread (Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    public void run() {
+        InputStream inp = null;
+        BufferedReader brinp = null;
+        DataOutputStream out = null;
+        try {
+            inp = clientSocket.getInputStream();
+            brinp = new BufferedReader(new InputStreamReader(inp));
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            return;
+        }
+        String line;
+        while (true) {
+            try {
+                line = brinp.readLine();
+                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+                    clientSocket.close();
+                    return;
+                } else {
+                    out.writeBytes(line + "\n\r");
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+    }
+
