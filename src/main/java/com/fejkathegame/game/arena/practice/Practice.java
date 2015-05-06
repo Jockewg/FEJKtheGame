fefb0001908ec53992305721acef1e2a30efa7a0
swartt <bobbyfeta@hotmail.com>
2015-05-06 17:25:02 +0200

    Added more targerts and spread them out

--- src/main/java/com/fejkathegame/game/arena/practice/Practice.java
+++ src/main/java/com/fejkathegame/game/arena/practice/Practice.java
@@ -11,144 +11,153 @@ import com.fejkathegame.game.entities.LevelObject;
 import com.fejkathegame.game.arena.tiles.AirTile;
 import com.fejkathegame.game.arena.tiles.SolidTile;
 import com.fejkathegame.game.arena.tiles.Tile;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Logger;
+
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 /**
- *
  * @author Swartt
  */
 public class Practice extends Level {
 
-   private TiledMap map;
+    private TiledMap map;
 
-   private Tile[][] tiles;
+    private Tile[][] tiles;
 
-   private ArrayList<LevelObject> players;
-   private ArrayList<PracticeTarget> targets;
-   
+    private ArrayList<LevelObject> players;
+    private ArrayList<PracticeTarget> targets;
 
-   /**
-    * Constructor for Arena, creates the playingfield and adds all players to
-    * the field
-    *
-    * @param name
-    * @param levelObject
-    * @throws SlickException
-    */
-   public Practice(String name, LevelObject levelObject) throws SlickException {
-      map = new TiledMap("src/main/resources/data/levels/" + name + ".tmx", "src/main/resources/data/img");
-      players = new ArrayList<LevelObject>();
-      targets = new ArrayList<PracticeTarget>();
 
-      addPlayer(levelObject);
-      addPracticeTargets();
-      loadTileMap();
-   }
+    /**
+     * Constructor for Arena, creates the playingfield and adds all players to
+     * the field
+     *
+     * @param name
+     * @param levelObject
+     * @throws SlickException
+     */
+    public Practice(String name, LevelObject levelObject) throws SlickException {
+        map = new TiledMap("src/main/resources/data/levels/" + name + ".tmx", "src/main/resources/data/img");
+        players = new ArrayList<LevelObject>();
+        targets = new ArrayList<PracticeTarget>();
 
-   /**
-    * populates the arena with tiles
-    */
-   public void loadTileMap() {
-      tiles = new Tile[map.getWidth() + 1][map.getHeight() + 1];
+        addPlayer(levelObject);
+        addPracticeTargets();
+        loadTileMap();
+    }
 
-      int layerIndex = map.getLayerIndex("CollisionLayer");
+    /**
+     * populates the arena with tiles
+     */
+    public void loadTileMap() {
+        tiles = new Tile[map.getWidth() + 1][map.getHeight() + 1];
 
-      if (layerIndex == -1) {
-         System.err.println("Map does not conatin CollisionLayer");
-         System.exit(0);
-      }
+        int layerIndex = map.getLayerIndex("CollisionLayer");
 
-      for (int x = 0; x < map.getWidth(); x++) {
-         for (int y = 0; y < map.getHeight(); y++) {
+        if (layerIndex == -1) {
+            System.err.println("Map does not conatin CollisionLayer");
+            System.exit(0);
+        }
 
-            int tileID = map.getTileId(x, y, layerIndex);
+        for (int x = 0; x < map.getWidth(); x++) {
+            for (int y = 0; y < map.getHeight(); y++) {
 
-            Tile tile = null;
+                int tileID = map.getTileId(x, y, layerIndex);
 
-            switch (map.getTileProperty(tileID, "tileType", "solid")) {
-               case "air":
-                  tile = new AirTile(x, y);
-                  break;
-               default:
-                  tile = new SolidTile(x, y);
-                  break;
+                Tile tile = null;
+
+                switch (map.getTileProperty(tileID, "tileType", "solid")) {
+                    case "air":
+                        tile = new AirTile(x, y);
+                        break;
+                    default:
+                        tile = new SolidTile(x, y);
+                        break;
+                }
+                tiles[x][y] = tile;
             }
-            tiles[x][y] = tile;
-         }
-      }
-   }
+        }
+    }
 
-   /**
-    * adds a player to the player array
-    *
-    * @param p
-    */
-   public void addPlayer(LevelObject p) {
-      players.add(p);
-   }
-   public void addPracticeTargets() {
-     
-       try {
-           PracticeTarget target1 = new PracticeTarget(160, 200);
-           targets.add(target1);
-           PracticeTarget target2 = new PracticeTarget(100, 200);
-           targets.add(target2);
-           PracticeTarget target3 = new PracticeTarget(200, 200);
-           targets.add(target3);
-           PracticeTarget target4 = new PracticeTarget(300, 200);
-           targets.add(target4);
-           PracticeTarget target5 = new PracticeTarget(500, 200);
-           targets.add(target5);
-//      PracticeTarget target6 = new PracticeTarget(160, 200);
-//      PracticeTarget target7 = new PracticeTarget(160, 200);
-//      PracticeTarget target8 = new PracticeTarget(160, 200);
-       } catch (SlickException ex) {
-           Logger.getLogger(Practice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-       } catch (IOException ex) {
-           Logger.getLogger(Practice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-       }
-   }
+    /**
+     * adds a player to the player array
+     *
+     * @param p
+     */
+    public void addPlayer(LevelObject p) {
+        players.add(p);
+    }
 
-   /**
-    * returns the player array
-    *
-    * @return
-    */
-   public ArrayList<LevelObject> getPlayers() {
-      return players;
-   }
-   
-   public ArrayList<PracticeTarget> getTargets() {
-       return targets;
-   }
-   
+    public void addPracticeTargets() {
 
-   /**
-    * @return a multidimensional array of tiles
-    */
-   public Tile[][] getTiles() {
-      return tiles;
-   }
+        try {
+            PracticeTarget target1 = new PracticeTarget(60, 70);
+            targets.add(target1);
+            PracticeTarget target2 = new PracticeTarget(100, 300);
+            targets.add(target2);
+            PracticeTarget target3 = new PracticeTarget(200, 50);
+            targets.add(target3);
+            PracticeTarget target4 = new PracticeTarget(400, 30);
+            targets.add(target4);
+            PracticeTarget target5 = new PracticeTarget(600, 50);
+            targets.add(target5);
+            PracticeTarget target6 = new PracticeTarget(400, 400);
+            targets.add(target6);
+            PracticeTarget target7 = new PracticeTarget(400, 200);
+            targets.add(target7);
+            PracticeTarget target8 = new PracticeTarget(750, 80);
+            targets.add(target8);
+            PracticeTarget target9 = new PracticeTarget(800, 435);
+            targets.add(target9);
+            PracticeTarget target10 = new PracticeTarget(160, 200);
+            targets.add(target10);
+        } catch (SlickException ex) {
+            Logger.getLogger(Practice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Practice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
 
-   /**
-    * Renders the arena
-    *
-    * @throws SlickException
-    */
-   public void render() throws SlickException {
-      map.render(0, 0, 0, 0, 36, 20);
+    /**
+     * returns the player array
+     *
+     * @return
+     */
+    public ArrayList<LevelObject> getPlayers() {
+        return players;
+    }
 
-      for (LevelObject p : players) {
-         p.render();
-      }
-      
-      for(LevelObject t : targets) {
-          t.render();
-      }
-     
-   }
+    public ArrayList<PracticeTarget> getTargets() {
+        return targets;
+    }
+
+
+    /**
+     * @return a multidimensional array of tiles
+     */
+    public Tile[][] getTiles() {
+        return tiles;
+    }
+
+    /**
+     * Renders the arena
+     *
+     * @throws SlickException
+     */
+    public void render() throws SlickException {
+        map.render(0, 0, 0, 0, 36, 20);
+
+        for (LevelObject p : players) {
+            p.render();
+        }
+
+        for (LevelObject t : targets) {
+            t.render();
+        }
+
+    }
 }
