55276f44bce3014eb896d28d7df86988df647686
Filip Ferm <ferm.filip@gmail.com>
2015-05-11 12:12:58 +0200

    change server and client

--- src/main/java/com/fejkathegame/client/Client.java
+++ src/main/java/com/fejkathegame/client/Client.java
@@ -1,52 +1,42 @@
 package com.fejkathegame.client;
 
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.io.*;
 
 /**
  * Created by Swartt on 2015-04-30.
  */
 public class Client {
-    protected Socket socket;
-    protected PrintWriter printWriter;
-    protected InetAddress address;
-    protected String playerName;
-    protected int port = 6112;
 
-    /**
-     * Constructor, requires A correct Ip address in order to connect
-     * @param ipAddress
-     * @param playerName
-     * @throws UnknownHostException
-     */
-    public Client(String ipAddress, String playerName) {
+    public static void main(String[] args) {
+        String serverName = "localhost";
+        int port = 6066;
         try {
-            address = InetAddress.getByName(ipAddress);
-        } catch (UnknownHostException e) {
+            System.out.println("Connecting to " + serverName
+                    + " on port " + port);
+            
+            Socket client = new Socket(serverName, port);
+            
+            System.out.println("Just connected to "
+                    + client.getRemoteSocketAddress());
+            
+            OutputStream outToServer = client.getOutputStream();
+            
+            DataOutputStream out
+                    = new DataOutputStream(outToServer);
+
+            out.writeUTF("Hello from "
+                    + client.getLocalSocketAddress());
+            
+            InputStream inFromServer = client.getInputStream();
+            
+            DataInputStream in
+                    = new DataInputStream(inFromServer);
+            
+            System.out.println("Server says " + in.readUTF());
+            client.close();
+        } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("incorrect IP address");
         }
-        this.playerName = playerName;
-    }
-
-    /**
-     * Connects a user to the server
-     */
-    public void connect() {
-        try {
-            System.out.println("creating new socket");
-            socket = new Socket(address, port);
-            /*socket.setKeepAlive(true);*/
-            printWriter = new PrintWriter(socket.getOutputStream(), true);
-            sendUserName();
-            printWriter.println("has connected");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-    public void sendUserName () {
-        printWriter.println(playerName);
     }
 }
--- src/main/java/com/fejkathegame/server/Server.java
+++ src/main/java/com/fejkathegame/server/Server.java
@@ -1,93 +1,59 @@
 package com.fejkathegame.server;
 
+import java.net.*;
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
 
 /**
  * Created by Swartt on 2015-04-30.
  */
-public class Server {
-    protected ServerSocket serverSocket;
-    protected Socket clientSocket;
-    protected BufferedReader bufferedReader;
-    protected String inputLine;
-    protected int port = 6112;
-    protected ArrayList<Socket> players = new ArrayList<>();
+public class Server extends Thread {
 
+    private ServerSocket serverSocket;
 
-    public Server() {
-        try {
-            serverSocket = new ServerSocket(port);
-        } catch (IOException e) {
-            System.out.println("server says: " + e);
-        }
-    }
-
-    public void handleConnection() {
-        while (true) {
-            try {
-                clientSocket = serverSocket.accept();
-                new ServerThread(clientSocket).run();
-            } catch (IOException e) {
-                System.out.println("derp");
-                System.out.println("handleConnection says: " + e);
-            }
-        }
-        }
-
-
-    public void sendToAllPlayers() {
-
-        for (Socket player : players) {
-            try {
-                DataOutputStream out = new DataOutputStream(player.getOutputStream());
-                out.writeChars("Test");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
-
-class ServerThread implements Runnable {
-    protected Socket clientSocket;
-
-    public ServerThread(Socket clientSocket) {
-        this.clientSocket = clientSocket;
-
-        Thread t = new Thread(this);
-        t.start();
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
     }
 
     public void run() {
-        InputStream inp;
-        BufferedReader brinp;
-        DataOutputStream out;
-        try {
-            inp = clientSocket.getInputStream();
-            brinp = new BufferedReader(new InputStreamReader(inp));
-            out = new DataOutputStream(clientSocket.getOutputStream());
-        } catch (IOException e) {
-            return;
-        }
-        String line;
         while (true) {
             try {
-                line = brinp.readLine();
-                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
-                    clientSocket.close();
-                    return;
-                } else {
-                    out.writeBytes(line + "\n\r");
-                    out.flush();
-                }
+                System.out.println("Waiting for client on port "
+                        + serverSocket.getLocalPort() + "...");
+                Socket server = serverSocket.accept();
+                System.out.println("Just connected to "
+                        + server.getRemoteSocketAddress());
+                
+                DataInputStream in
+                        = new DataInputStream(server.getInputStream());
+                
+                System.out.println(in.readUTF());
+                
+                DataOutputStream out
+                        = new DataOutputStream(server.getOutputStream());
+                
+                out.writeUTF("Thank you for connecting to "
+                        + server.getLocalSocketAddress() + "\nGoodbye!");
+                
+                server.close();
+                
+            } catch (SocketTimeoutException s) {
+                System.out.println("Socket timed out!");
+                break;
             } catch (IOException e) {
                 e.printStackTrace();
-                return;
+                break;
             }
         }
     }
-}
 
+    public static void main(String[] args) {
+        int port = 6066;
+        try {
+            Thread t = new Server(port);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
