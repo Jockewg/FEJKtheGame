e97776e24e345482d415ed8cce69e3ec6428b8b9
Filip Ferm <ferm.filip@gmail.com>
2015-05-11 12:58:10 +0200

    made client runnable only

--- src/main/java/com/fejkathegame/client/Client.java
+++ src/main/java/com/fejkathegame/client/Client.java
@@ -8,38 +8,6 @@ import java.io.*;
  */
 public class Client implements Runnable{
 
-    public static void main(String[] args) {
-        String serverName = "localhost";
-        int port = 6112;
-        try {
-            System.out.println("Connecting to " + serverName
-                    + " on port " + port);
-            
-            Socket client = new Socket(serverName, port);
-            
-            System.out.println("Just connected to "
-                    + client.getRemoteSocketAddress());
-            
-            OutputStream outToServer = client.getOutputStream();
-            
-            DataOutputStream out
-                    = new DataOutputStream(outToServer);
-
-            out.writeUTF("Hello from "
-                    + client.getLocalSocketAddress());
-            
-            InputStream inFromServer = client.getInputStream();
-            
-            DataInputStream in
-                    = new DataInputStream(inFromServer);
-            
-            System.out.println("Server says " + in.readUTF());
-            client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override
     public void run() {
         String serverName = "localhost";
