6a67dc638aa75ab869b0d4e802ccb37de34bbde6
Joakim Widerberg <Joakim_Widerberg@hotmail.com>
2015-05-12 15:51:45 +0200

    Add Button class & adopted into Menu

--- src/main/java/com/fejkathegame/menu/MenuState.java
+++ src/main/java/com/fejkathegame/menu/MenuState.java
@@ -1,6 +1,7 @@
 package com.fejkathegame.menu;
 
 import com.fejkathegame.game.Main;
+import com.fejkathegame.menu.button.Button;
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -35,13 +36,16 @@ public class MenuState extends BasicGameState {
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         g.setBackground(Color.decode("#655d5d"));
         menu.render();
+        g.drawString(Mouse.getX()+ "", 100, 100);
+        g.drawString(Mouse.getY()+ "", 100, 120);
+        
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
         input = gc.getInput();
         int mouseX = Mouse.getX();
-        int mouseY = Mouse.getY();
+        int mouseY = gc.getHeight() - Mouse.getY();
         checkIfButtonIsPressed(mouseX, mouseY, input, sbg);
 
     }
@@ -58,19 +62,35 @@ public class MenuState extends BasicGameState {
      * @param sbg context of the game that calls for new states
      */
     public void checkIfButtonIsPressed(int x, int y, Input i, StateBasedGame sbg) {
-        if((x > 250 && x < 400) && (y < 200 && y > 125)) {
+//        if((x > 250 && x < 400) && (y < 200 && y > 125)) {
+//            if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+//                sbg.enterState(Main.VERSUSSTATE);
+//            }
+//        } else if((x > 550 && x < 700) && (y < 200 && y > 125)) {
+//            if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+//                sbg.enterState(Main.HOSTSTATE);
+//            }
+//        } else if((x > 325 && x < 500) && (y < 100 && y > 25)) {
+//            if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+//                sbg.enterState(Main.LEVELSELECTSTATE);
+//            }
+//        }
+        
+        if(menu.getButtons()[0].onHover(x, y)){
             if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
                 sbg.enterState(Main.VERSUSSTATE);
             }
-        } else if((x > 550 && x < 700) && (y < 200 && y > 125)) {
+        }else if(menu.getButtons()[1].onHover(x, y)){
             if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
                 sbg.enterState(Main.HOSTSTATE);
             }
-        } else if((x > 325 && x < 500) && (y < 100 && y > 25)) {
+        }else if(menu.getButtons()[2].onHover(x, y)){
             if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
                 sbg.enterState(Main.LEVELSELECTSTATE);
             }
         }
+            
+        
     }
 
 }
