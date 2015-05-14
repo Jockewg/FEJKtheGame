08498802e356fd274bf10e7eb9da0beda34f37e1
swartt <bobbyfeta@hotmail.com>
2015-05-14 13:23:02 +0200

    Updated highscore

--- src/main/java/com/fejkathegame/game/arena/maps/highscore/HighscoreAdapter.java
+++ src/main/java/com/fejkathegame/game/arena/maps/highscore/HighscoreAdapter.java
@@ -7,25 +7,37 @@ import java.util.Properties;
  * Created by Swartt on 2015-05-13.
  */
 public class HighscoreAdapter {
-    Properties properties = new Properties();
+    Properties properties;
     OutputStream outputStream = null;
     InputStream inputStream = null;
     String configFilePath = "config.prop";
 
+
+    /**
+     * saves the score to a map in the config.prop file
+     * @param map
+     * @param score
+     */
     public void saveScore(int map, int score) {
-        if (readScore(map) > score) {
+        int prevscore = readScore(map);
+        System.out.println("prevscore is: "+prevscore);
+
+        if (prevscore > score) {
 
 
             String Smap = String.valueOf(map);
+            System.out.println("String version of map is: "+Smap);
             String Sscore = String.valueOf(score);
-
+            System.out.println("String version of score is: " + Sscore);
             try {
                 outputStream = new FileOutputStream(configFilePath);
 
                 properties.setProperty(Smap, Sscore);
 
                 properties.store(outputStream, null);
-                System.out.println("the new highscore is: " + score);
+                outputStream.close();
+                int newScore = readScore(map);
+                System.out.println("the new highscore is: " + newScore);
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -34,6 +46,12 @@ public class HighscoreAdapter {
             }
         }
     }
+
+    /**
+     * Fetches the score of a map, if no score is present, it returns {@code Integer.MAX_VALUE}
+     * @param map the map for which score to fetch
+     * @return
+     */
     public int readScore(int map) {
         String Smap = String.valueOf(map);
 
@@ -41,17 +59,19 @@ public class HighscoreAdapter {
 
         try {
             inputStream = new FileInputStream(configFilePath);
+            properties.load(inputStream);
             if (properties.getProperty(Smap) != null) {
                 score = Integer.valueOf(properties.getProperty(Smap));
-                System.out.println("the highscore for " + map + "was: " + score);
+                System.out.println(score);
             } else {
                 System.out.println("value was null");
                 score = Integer.MAX_VALUE;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-        finally {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
             if (inputStream != null) {
                 try {
                     inputStream.close();
